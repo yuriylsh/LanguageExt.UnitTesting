6d64ca89ab044667d858acbd264803d4b21dfa2d
Xavier Lecoustillier <xavier.lecoustillier@alcuin.com>
2018-07-22 14:46:08 +0200

    Add EitherExtensions

---
+++ LanguageExt.UnitTesting/EitherExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LanguageExt.UnitTesting
+{
+    public static class EitherExtensions
+    {
+        public static void ShouldBeRight<TLeft, TRight>(this Either<TLeft, TRight> @this, Action<TRight> rightValidation)
+            => @this.Match(rightValidation, _ => throw new Exception("Expected Right, got Left instead."));
+
+        public static void ShouldBeLeft<TLeft, TRight>(this Either<TLeft, TRight> @this, Action<TLeft> failValidation)
+            => @this.Match(_ => throw new Exception("Expected Left, got Right instead."), failValidation);
+    }
+}
