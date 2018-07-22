120858f618cc2da6fb7ad82b13bdd1f79f7d6b30
Xavier Lecoustillier <xavier.lecoustillier@alcuin.com>
2018-07-22 15:54:29 +0200

    Add unit tests

---
+++ LanguageExt.UnitTesting.Tests/ValidationExtensions_ShouldBeSuccess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+
+namespace LanguageExt.UnitTesting.Tests
+{
+    public class ValidationExtensions_ShouldBeSuccess
+    {
+        [Fact]
+        public void ShouldBeSuccess_Should_Throw_If_Fail()
+        {
+            Validation<Exception, string> validation = new Exception();
+
+            Assert.Throws<Exception>(() => validation.ShouldBeSuccess(x => { }));
+        }
+
+        [Fact]
+        public void ShouldBeSuccess_Should_Execute_SuccessAction()
+        {
+            Validation<Exception, string> validation = "test";
+
+            var result = false;
+            validation.ShouldBeSuccess(x => result = true);
+            Assert.True(result);
+        }
+    }
+}
